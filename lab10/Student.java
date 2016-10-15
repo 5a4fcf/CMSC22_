12b7b082cb1cc5abc9fe011ff2ab886f5a8ab11a
Iris Desabille <iris.desabille@gmail.com>
2016-10-15 23:01:24 +0800

    Update and rename Student.java to lab10/Student.java

--- lab10/Student.java
+++ lab10/Student.java
@@ -4,7 +4,7 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 /**
- * Created by next on 10/15/2016.
+ * Created by Arsolon,Cugal,Desabille on 10/15/2016.
  */
 public class Student {
     private String studentNumber;
